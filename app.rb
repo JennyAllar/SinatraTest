e8cd79124845a8b480af12cc385e406b4d8a291f
Seth Musulin <sethmusulin@gmail.com>
2014-03-21 16:38:42 -0600

    Added field to add new item
    Connected the pipes, but not yet able to add items to the list
    
    Signed-off-by: Jenny Allar <jennyallar@gmail.com>

--- app.rb
+++ app.rb
@@ -11,3 +11,13 @@ get '/items' do
   @user_input = params[:filter]
   erb :items_list
 end
+
+
+get '/items/new' do
+  erb :new_items
+end
+
+post '/items' do
+  new_item = params[:new_item]
+  erb :new_items
+end
